653c728c86f1070970acf6f7214bf47c13f397ed
batrick-swaistan <batrickswaistan@gmail.com>
2024-09-11 11:41:30 +0530

    Responsive

--- src/Components/ProfileDashboard/Profilebar-3/UserBarThree.jsx
+++ src/Components/ProfileDashboard/Profilebar-3/UserBarThree.jsx
@@ -19,76 +19,80 @@ const UserBarThree = ({ data, setData, loading }) => {
 
   return (
     <>
-      <div className="user-bar-3 flex-1 mx-2 ">
-        {loading ? (
-          <div className="mx-4">
-            <RectangleSkeleton />
-          </div>
-        ) : (
-          <>
-            <div className="user-project-section ">
-              <div className="project-title flex flex-row align-items-center justify-content-between">
-                <h2>Projects</h2>
-
-                {data?.projects && data?.projects?.length < 2 ? (
-                  <>
-                    <span
-                      className="add-icon flex flex-column align-items-center justify-content-center cursor-pointer"
-                      onClick={() => {
-                        setVisible(true);
-                        SetDialog("add-project");
-                      }}
-                    >
-                      <i className="pi pi-plus"></i>
-                    </span>
-                  </>
-                ) : (
-                  <></>
-                )}
+      {data && (
+        <>
+          <div className="user-bar-3 flex-1 mx-2 ">
+            {loading ? (
+              <div className="mx-4">
+                <RectangleSkeleton />
               </div>
+            ) : (
+              <>
+                <div className="user-project-section ">
+                  <div className="project-title flex flex-row align-items-center justify-content-between">
+                    <h2>Projects</h2>
 
-              {data?.projects?.length > 0 ? (
-                <>
-                  {data?.projects?.map((project, index) => (
-                    <>
-                      <div className="project-details">
-                        <div className="project-title flex flex-row align-items-center justify-content-between">
-                          <h3>{project.title}</h3>
-
-                          <span
-                            className="edit-icon cursor-pointer 1 flex flex-column justify-content-center align-items-center"
-                            onClick={() => {
-                              setSelectedProject(project);
-                              setVisible(true);
-                              SetDialog("edit-project");
-                            }}
-                          >
-                            <i className="pi pi-pencil "></i>
-                          </span>
-                        </div>
-                        <span className="project-description">
-                          {project.description}
+                    {data?.projects && data?.projects?.length < 2 ? (
+                      <>
+                        <span
+                          className="add-icon flex flex-column align-items-center justify-content-center cursor-pointer"
+                          onClick={() => {
+                            setVisible(true);
+                            SetDialog("add-project");
+                          }}
+                        >
+                          <i className="pi pi-plus"></i>
                         </span>
-                      </div>
+                      </>
+                    ) : (
+                      <></>
+                    )}
+                  </div>
+
+                  {data?.projects?.length > 0 ? (
+                    <>
+                      {data?.projects?.map((project, index) => (
+                        <>
+                          <div className="project-details">
+                            <div className="project-title flex flex-row align-items-center justify-content-between">
+                              <h3>{project.title}</h3>
+
+                              <span
+                                className="edit-icon cursor-pointer 1 flex flex-column justify-content-center align-items-center"
+                                onClick={() => {
+                                  setSelectedProject(project);
+                                  setVisible(true);
+                                  SetDialog("edit-project");
+                                }}
+                              >
+                                <i className="pi pi-pencil "></i>
+                              </span>
+                            </div>
+                            <span className="project-description">
+                              {project.description}
+                            </span>
+                          </div>
+                        </>
+                      ))}
                     </>
-                  ))}
-                </>
-              ) : (
-                <>
-                  <span className="no-project-content">
-                    Please share details about up to 2 of your projects,
-                    including a brief description, the technologies used, and
-                    your role in each. Highlight any challenges you faced and
-                    how you overcame them. This information helps us appreciate
-                    your expertise and the value you've brought to your
-                    projects.
-                  </span>
-                </>
-              )}
-            </div>
-          </>
-        )}
-      </div>
+                  ) : (
+                    <>
+                      <span className="no-project-content">
+                        Please share details about up to 2 of your projects,
+                        including a brief description, the technologies used,
+                        and your role in each. Highlight any challenges you
+                        faced and how you overcame them. This information helps
+                        us appreciate your expertise and the value you've
+                        brought to your projects.
+                      </span>
+                    </>
+                  )}
+                </div>
+              </>
+            )}
+          </div>
+        </>
+      )}
 
       <div className="userbartwo-dialog">
         <Dialog
